3237f73293a07da97fcce53dfd48882bc6eb656d
jagadishpatil <jagadishpatil231@gmail.com>
2021-04-28 23:26:15 +0530

    initialized firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBStyT6tCQ_PXb9Rvl7RPuiwOR7zCJUF68",
+  authDomain: "evernote-clone-28fef.firebaseapp.com",
+  projectId: "evernote-clone-28fef",
+  storageBucket: "evernote-clone-28fef.appspot.com",
+  messagingSenderId: "1096022932178",
+  appId: "1:1096022932178:web:2a10e15236dce46b880b0b",
+  measurementId: "G-325ZPTJ5YW"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
