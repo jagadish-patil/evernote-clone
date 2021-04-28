c987ff2c0ee07301a444abb7a69b7ed0dfa8ba57
jagadishpatil <jagadishpatil231@gmail.com>
2021-04-28 23:54:50 +0530

    empty note

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
 import React from 'react';
 
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
 class App extends React.Component{
+
+  constructor(){
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null
+    };
+  }
 
   render() {
     return (<div>Hello World!</div>);
   }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(_doc => {
+          const data = _doc.data();
+          data['id'] = _doc.id;
+          return data;
+        });
+        console.log(notes);
+        this.setState({ notes: notes });
+      });
+
+  }
+
 }
 
 export default App;
